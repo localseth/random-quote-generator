bfc41daa57e5a86cf5b6660ec2eff94fca8dc26f
Seth M <97685638+localseth@users.noreply.github.com>
2022-03-18 14:58:15 -0500

    added functions for: displaying quote in HTML changing background when new quote is generated automatically generating a new quote after 10 seconds updated css to show #tag info

--- js/script.js
+++ js/script.js
@@ -8,54 +8,108 @@ Project 4 - Random Quote Generator
   // Reach out in your Slack community
 
 /*** 
- * `quotes` array 
+ * Collection of quotes organized as objects inside an array
 ***/
 const quotes = [
   {
    quote: 'Not all those who wander are lost',
    source: 'J.R.R. Tolkien',
    citation: 'The Fellowship of the Ring',
-   year: 'Pub. 1954' 
+   year: 'Pub. 1954',
+   tag: 'literature'
   },
   {
     quote: "When two people meet, each one is changed by the other so you've got two new people.",
-    source: 'John Steinbeck'
+    source: 'John Steinbeck',
+    tag: 'literature'
    },
    {
     quote: 'In the councils of government, we must guard against the acquisition of unwarranted influence, whether sought or unsought, by the military-industrial complex.',
     source: 'Dwight D. Eisenhower',
     citation: 'Farewell Address',
-    year: 1961
+    year: 1961,
+    tag: 'politics'
    },
    {
     quote: 'He who is not contented with what he has, would not be contented with what he would like to have.',
-    source: 'Socrates'
+    source: 'Socrates',
+    tag: 'philosophy'
    },
    {
     quote: 'Only those who will risk going too far can possibly find out how far one can go.',
     source: 'T.S. Eliot',
     citation: 'Preface to <i>Transit of Venus</i> by Harry Crosby',
-    year:  'Pub. 1931'
+    year:  'Pub. 1931',
+    tag: 'literature'
    }
 ]
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function chooses a random object from the 'quotes' array and returns it
 ***/
 
 function getRandomQuote (arr){
   randomNumber = Math.floor(Math.random() * arr.length);
-  console.log(randomNumber + ' ' + arr[randomNumber]);
+  console.log(randomNumber, arr[randomNumber]);
   return arr[randomNumber];
 }
 
 getRandomQuote(quotes);
 
 /***
- * `printQuote` function
+ * `printQuote` function targets the properties inside the 'quotes' array and interpolates them inside HTML tags
+ * It then searches for additional properties (citation and year) and adds those to the quoteHTML string
+ * Finally it closes the HTML code with a closing </p> tag, logs the quote to the console, then returns the entire quoteHTML string
 ***/
 
+function printQuote () {
+  let randomQuote = getRandomQuote(quotes);
+  let quoteHTML = `
+  <p class="quote">${randomQuote.quote}</p>
+  <p class="source">${randomQuote.source}`;
+  if (randomQuote.citation) {
+    quoteHTML += `<span class="citation">${randomQuote.citation}</span>`
+  }
+  if (randomQuote.year){
+    quoteHTML += `<span class="year">${randomQuote.year}</span>`
+  }
+  if (randomQuote.tag){
+    quoteHTML += `<span class="tag">${randomQuote.tag}</span>`
+  }
+  quoteHTML += "</p>";
+  console.log(quoteHTML);
+  return document.getElementById('quote-box').innerHTML = quoteHTML;
+}
 
+/***
+ * Assign the "Show another quote" button to a variable in order to simplify the code for the
+ * automatic background color changer and interval timer
+***/
+const newQuote = document.getElementById('load-quote');
+
+/***
+ * function to change background color
+***/
+const randomValue = () => Math.floor(Math.random() * 256);
+
+function randomColor() {
+  let color= `rgb( ${randomValue()}, ${randomValue()}, ${randomValue()})`
+  return color;
+}
+
+newQuote.addEventListener("click", () => {
+  const body = document.body;
+  body.style.background = randomColor();
+});
+
+/***
+ * function to automatically refresh the quote
+***/
+function clickNew () {
+  newQuote.click();
+}
+
+const freshQuote = setInterval(clickNew, 10000);
 
 /***
  * click event listener for the print quote button
