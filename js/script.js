94484feaa12fa0e0c14bf8d648036362fa7c20b5
Seth M <97685638+localseth@users.noreply.github.com>
2022-03-18 11:40:51 -0500

    Built array of objects for quotes and created a function to generate a random quote

--- js/script.js
+++ js/script.js
@@ -10,14 +10,46 @@ Project 4 - Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quotes = [
+  {
+   quote: 'Not all those who wander are lost',
+   source: 'J.R.R. Tolkien',
+   citation: 'The Fellowship of the Ring',
+   year: 'Pub. 1954' 
+  },
+  {
+    quote: "When two people meet, each one is changed by the other so you've got two new people.",
+    source: 'John Steinbeck'
+   },
+   {
+    quote: 'In the councils of government, we must guard against the acquisition of unwarranted influence, whether sought or unsought, by the military-industrial complex.',
+    source: 'Dwight D. Eisenhower',
+    citation: 'Farewell Address',
+    year: 1961
+   },
+   {
+    quote: 'He who is not contented with what he has, would not be contented with what he would like to have.',
+    source: 'Socrates'
+   },
+   {
+    quote: 'Only those who will risk going too far can possibly find out how far one can go.',
+    source: 'T.S. Eliot',
+    citation: 'Preface to <i>Transit of Venus</i> by Harry Crosby',
+    year:  'Pub. 1931'
+   }
+]
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote (arr){
+  randomNumber = Math.floor(Math.random() * arr.length);
+  console.log(randomNumber + ' ' + arr[randomNumber]);
+  return arr[randomNumber];
+}
 
+getRandomQuote(quotes);
 
 /***
  * `printQuote` function
